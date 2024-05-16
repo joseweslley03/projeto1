2aedf0292f40d595017d81a4d37da9ea22309e7a
joseweslley03 <weslleyphb03@gmail.com>
2024-05-15 21:26:17 -0300

    Todo atualizado

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
+from venv import create
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=200)
+
+
+class Item(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    todo = models.ForeignKey(Todo, on_delete=models.CASCADE,related_name="items")
+    text = models.TextField()
+    complete = models.BooleanField(default=False)
+
+
--- settings/urls.py
+++ settings/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from main import views
+from main.views import index, show
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', index),
+    path("<int:id>/", show),
 ]
