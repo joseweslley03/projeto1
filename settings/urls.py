4be33af317366f5db11e0995eb7f8fdd7b0e0363
joseweslley03 <weslleyphb03@gmail.com>
2024-05-31 21:13:37 -0300

    Mudança projeto

--- settings/urls.py
+++ settings/urls.py
@@ -17,10 +17,16 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from main.views import index, show
+from main.views import index, show, store, destroy, destroy_item, put
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path("<int:id>/", show, name='show'),
+    path('nova/tarefa/', store, name='store'),
+    path("<int:todo_id>/novo/item/", store, name='store-item'),
+    path('<int:todo_id>/excluir/tarefa/', destroy, name='destroy'),
+    path('<int:todo_id>/<int:item_id>/excluir/item/', destroy_item, name='destroy-item'),
+    path('item/<int:item_id>/edit/', put, name='put-item'),
+
 ]
